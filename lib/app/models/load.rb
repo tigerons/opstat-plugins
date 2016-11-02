969042594fe5d88a1cf93d417f322809507df082
Krzysztof Tomczyk <czytom@gmail.com>
2016-11-02 13:00:01 +0100

    rewrite to use abstract charts class

--- lib/app/models/load.rb
+++ lib/app/models/load.rb
@@ -1,5 +1,6 @@
 class Load
   include MongoMapper::Document
+  include Graphs::LineChart
   set_collection_name "opstat.reports"
   key :timestamp, Time
   timestamps!
@@ -11,24 +12,7 @@ class Load
   end
 
   def self.load_chart(options)
-    load_data = {
-               :value_axes => [
-                          { 
-                            :name => "valueAxis1",
-                            :title => 'Load average',
-                            :position => 'left',
-                            :min_max_multiplier => 1,
-                            :stack_type => 'none',
-                            :include_guides_in_min_max => 'false',
-                            :grid_alpha => 0.1
-                          }
-                        ],
-               :graph_data => [],
-               :graphs => [],
-               :title => "Host load average",
-	       :category_field => 'time',
-               :title_size => 20
-             }
+    chart = self.chart_structure({:title => "Host load average", :value_axis => { :title => "Memory size in KB"}})
 
     #CHOOSE HERE which
     # TODO ADD ALL TYPES
@@ -36,12 +20,8 @@ class Load
     graphs = [ :load_1m, :load_5m, :load_15m ]
 
 #TODO cpu and here - sort by timestamp
-    load_data[:graph_data] = Load.where( {:timestamp => { :$gte => options[:start],:$lt => options[:end]}, :host_id => options[:host_id], :plugin_id => options[:plugin_id] }).fields(:load_1m, :load_5m, :load_15m, :timestamp).order(:timetamp).all
+    chart[:graph_data] = Load.where( {:timestamp => { :$gte => options[:start],:$lt => options[:end]}, :host_id => options[:host_id], :plugin_id => options[:plugin_id] }).fields(:load_1m, :load_5m, :load_15m, :timestamp).order(:timetamp).all
 
-    graphs.each do |graph|
-      #TODO value_axis
-      load_data[:graphs] << { :value_axis => 'valueAxis1', :value_field => graph, :balloon_text => "[[title]]: ([[value]])", :line_thickness => 1, :line_alpha => 1, :fill_alphas => 0, :graph_type => 'line', :title => graph }
-    end
     cores_total = Facts.get_fact({:name => "processorcount", :host_id => options[:host_id], :plugin_id => options[:plugin_id]})
     unless cores_total.nil?
       cores_total = cores_total.value.to_i
@@ -87,34 +67,29 @@ class Load
         :fill_alpha => 0.1,
         :position => 'bottom'}]
     end
-    load_data[:guides] = guides
-    load_data
+    chart[:guides] = guides
+    chart
   end
   
-  def self.graphs_defaults
-    [
-     { :value_field => "Load_15m",
-       :hidden => false,
-       :line_color => "#FF0000",
-       :line_thickness => 3,
-       :title => "Load 15 minutes average"},
-     { :value_field => "Load_5m",
-       :hidden => false,
-       :line_color => "#FFFF00",
-       :line_thickness => 3,
-       :title => "Load 5 minutes average"},
-     { :value_field => "Load_1m",
-       :hidden => false,
-       :line_color => "#FFaa00",
-       :line_thickness => 3,
-       :title => "Load 1 minute average"}
-    ]
-  end
-
-  def self.axes_defaults
-    {
-      :value_axis => {:title => 'System load average'}
-    }
+  def self.graphs
+    { 
+      :load_15m => { 
+        :hidden => false,
+        :line_color => "#FF0000",
+        :line_thickness => 3,
+        :title => "Load 15 minutes average"},
+       :load_5m => {
+         :hidden => false,
+         :line_color => "#FFFF00",
+         :line_thickness => 3,
+         :title => "Load 5 minutes average"},
+       :load_1m => {
+         :hidden => false,
+         :line_color => "#FFaa00",
+         :line_thickness => 3,
+         :title => "Load 1 minute average"}
+     }
   end
 end
-Load.ensure_index( [ [:timestamp, 1], [:hostname, 1] , [:ip_address,1], [:cpus, 1] ] )
+
+Load.ensure_index( [ [:timestamp, 1], [:hostname, 1] , [:ip_address,1] ] )
