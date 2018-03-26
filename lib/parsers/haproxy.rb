6680d103514ae2ab76a7fb47d61a25c70c35bdf3
tigerons <37334768+tigerons@users.noreply.github.com>
2018-03-26 13:00:53 +0200

    Update haproxy.rb

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -1,25 +1,18 @@
-require 'open-uri'
 require 'csv'
 module Opstat
 module Parsers
 
-	class Haproxy
- 	 include Opstat::Logging
+        class Haproxy
+         include Opstat::Logging
 
-		def parse_data(data)
- 
-                 source = File.read('/home/tajger/Dokumenty/opstat-plugins/haproxy_stat.txt')          
-		 csv_records = CSV.parse(source, { headers: true, header_converters: :symbol, converters: :all })
-		 @hashed_data = csv_records.map { |data| data.to_hash }.last
+                def parse_data(data_parse)
+                 csv_records = CSV.parse(data_parse.join, { headers: true, header_converters: :symbol, converters: :all })
+                 @hashed_data = csv_records.map { |data| data.to_hash }.last
 
-		end 
- 
-
-	 end 
+                end
+         end
         end
 end
- 
-
 
 
 
