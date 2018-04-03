32a536845195ba150eb30d417f4a22e3fed02f7d
tigerons <37334768+tigerons@users.noreply.github.com>
2018-04-03 15:01:32 +0200

    add test haproxy

--- lib/plugins/network.rb
+++ lib/plugins/network.rb
@@ -1,21 +1,20 @@
-module Opstat
-module Plugins
-class Network < Task
-  STAT_FILE = "/proc/net/dev"
-
-  def initialize (name, queue, config)
-    super(name, queue, config)
-    self
-  end
-
-  def parse
-    report = []
-    File.open(STAT_FILE).each do |line|
-        report << line
-    end
-    return report
-  end
-
+describe 'Haproxy' do
+  describe 'Parsers' do
+    describe 'haproxy parser' do 
+      before :each do
+        Opstat::Plugins.load_parser("haproxy")
+	  @haproxy_parser = Opstat::Parsers::Haproxy.new
+      end
+      
+it 'returns report with all parsed params when input data are correct' do 
+       haproxy_data = File.readlines ('./spec/fixtures/parser_haproxy_correct.txt')
+      result_expected = [{:pxname => "backend-tomcat-magazyn", :svname => "BACKEND", :qcur => 0, :qmax => 0, :scur => 1, :smax => 6, :slim => 500, :stot => 173604, :status => "UP", :bin => 16602280, :bout => 225978515}]
+      expect(@haproxy_parser.parse_data(haproxy_data)).to eq result_expected 
 end
-end
-end
+it 'return empty array where input data are corrupted' do 
+     haproxy_data = File.readlines ('./spec/fixtures/parser_haproxy_corrupted.txt')
+     expect(@haproxy_parser.parse_data(haproxy_data)).to eq [] 
+ end 
+    end 
+  end 
+end 
