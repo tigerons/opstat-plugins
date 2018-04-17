503122a144df4a705b04cb85be37d82486248a0c
tigerons <37334768+tigerons@users.noreply.github.com>
2018-04-17 09:30:08 +0200

    Update haproxy.rb

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -4,35 +4,29 @@ module Parsers
 
   class Haproxy
     include Opstat::Logging
-  
+    
     def parse_data(data_parse)
-      csv_records = CSV.parse(data_parse.join, { headers: true, header_converters: :symbol, converters: :all })
-	
-      @hashed_data = csv_records.map { |data| data.to_hash }
-      begin
-	return [{
-	  :pxname => @hashed_data[:pxname],
-	  :svname => @hashed_data[:svname],
-	  :qcur => @hashed_data[:qcur],
-	  :qmax => @hashed_data[:qmax],
-	  :scur => @hashed_data[:scur],
-	  :smax => @hashed_data[:smax],
-	  :slim => @hashed_data[:slim],
-	  :stot => @hashed_data[:stot],
-	  :status => @hashed_data[:status],
-	  :bin => @hashed_data[:bin],
-	  :bout => @hashed_data[:bout],
-	}] 
-      rescue Exception => e
-	return [] 
-      end  
+      white_headers = %w(svname bin bout)
+      reports = []
+       hashed_data = {}
+      CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}) do |row|
+      hashed_data = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
+      reports <<  { 
+	:_pxname => hashed_data[:_pxname],
+	:svname =>  hashed_data[:svname],
+	:qcur => hashed_data[:qcur],
+	:qmax => hashed_data[:qmax],
+	:scur => hashed_data[:scur],
+	:smax => hashed_data[:smax],
+	:slim => hashed_data[:slim],
+	:stot => hashed_data[:stot],
+	:bin => hashed_data[:bin],
+	:bout => hashed_data[:bout]
+       }
     end
+      return reports
 end
 end 
-end 
-
-
-
-
-
+end
+end
 
