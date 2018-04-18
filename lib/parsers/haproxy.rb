3e7095fed6f506582c03ccb9f9f8ff9fc614905a
tigerons <37334768+tigerons@users.noreply.github.com>
2018-04-18 10:02:52 +0200

    Update haproxy.rb

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -8,7 +8,6 @@ module Parsers
     def parse_data(data_parse)
       white_headers = [:_pxname, :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout]
       hashed_data = {} 
-      reports = [] 
       CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}) do |row|
 	hashed_data = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
       end 
