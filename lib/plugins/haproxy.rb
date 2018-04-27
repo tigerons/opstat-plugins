99bfbbee6d6ddb978d72def0814ab2ffdcf8d7c6
tigerons <37334768+tigerons@users.noreply.github.com>
2018-04-27 12:23:34 +0200

    Update haproxy.rb

--- lib/plugins/haproxy.rb
+++ lib/plugins/haproxy.rb
@@ -3,15 +3,19 @@ module Opstat
 module Plugins
 
   class Haproxy < Task
-    url = 'http://haproxy01-staging:1937/;up/stats;csv;norefresh' 
+    def initialize (name, queue, config)
+    super(name, queue, config)
+    @haproxy_url = "#{config['url']}/;up/stats;csv;norefresh'"
+    self
+    end
+    
     def parse
       report = [] 
-      source = open(url).each do |line|
-	report << line 
-      end 
-    return report 
+      source = open(@haproxy_url).each do |line|
+      report << line 
+    end 
+     return report 
     end 
   end
 end
 end 
- 
