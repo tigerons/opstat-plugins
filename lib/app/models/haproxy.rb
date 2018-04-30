908903b2b4bb9f377ed074e27bdcae0b8e0167ec
tigerons <37334768+tigerons@users.noreply.github.com>
2018-04-30 11:43:13 +0200

    Update haproxy.rb

--- lib/app/models/haproxy.rb
+++ lib/app/models/haproxy.rb
@@ -1,79 +1,55 @@
-=begin
 class Haproxy
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
   include Mongoid::Timestamps
+  include Graphs::AreaNotStackedChart
   store_in collection: "opstat.reports"
   field :timestamp, type: DateTime
-  field :session_total, type: Integer
-  field :session_transmit, type: Integer 
-  index({timestamp: 1, host_id: 1, plugin_id: 1}, {background: true}) 
-  
+  index({timestamp: 1, host_id: 1, plugin_id: 1}, {background: true})
+
   def self.chart_data(options = {})
     charts = []
-    charts << self.haproxy_charts(options)
+    charts << self.haproxy_chart(options)
     return charts
   end
 
 
-  def self.haproxy_charts(options)
-    charts = []
-   Haproxy.where(:timestamp.gte => options[:start]).
+  def self.haproxy_chart(options)
+   chart = self.chart_structure({:title => 'Haproxy test', :value_axis => { :title => "Session per second"}})
+
+   chart[:graph_data] = Haproxy.where(:timestamp.gte => options[:start]).
             where(:timestamp.lt => options[:end]).
-	    where(:host_id => options[:host_id]).
-	    where(:plugin_id => options[:plugin_id]).
-	    order_by(timestamp: :asc).group_by{|u| u.interface}.each_pair do |interface, values|
-      charts << self.haproxy_chart(interface, values)
-    end
-    return charts
-  end
+            where(:host_id => options[:host_id]).
+            where(:plugin_id => options[:plugin_id]).order_by(timestamp: :asc)
+   guides = []
+   unless  host_facts.nil?
+   guide = {}
+   require 'ruby-units'
+      guide[:value] = 'forntend-http'
+      guide[:value_axis] = 'valueAxis1'
+      guide[:line_color] = "#00FFaa33"
+      guide[:line_thickness] = 1
+      guide[:dash_length] = 5
+      guide[:label] = "Total physical"
+      guide[:inside] = true
+      guide[:line_alpha] = 1
+      guide[:position] = 'bottom'
+      guides << guide
 
-=begin   
- def self.haprox_chart(interface, values)
-    chart_data = self.chart_structure({:title => "Network traffic for #{interface}", :value_axis => { :title => "Network traffic for #{interface}"}})
-    
-    prev = nil
-    values.each do |value|
-      if prev.nil? then
-        prev = value
-        next
-      end
-      time_diff = value[:timestamp].to_i - prev[:timestamp].to_i
-      chart_data[:graph_data] << {
-        "timestamp" => value[:timestamp],
-        "sesion_per_second" => ((value[:stot].to_i)/time_diff).to_i
-      }
-      prev = value 
-      
-    end
-
-    chart_data
   end
-=end  
-	
+   chart[:guides] = guides
+   chart
+
+ end
+
   def self.graphs
     {
- 
-      :session_per_second => { :line_color => '#00FF00'}, 
-      :_pxname => { :line_color => '#00FF00'}, 
-      :svname  => { :line_color => '#00FF00'},
-      :qcur => { :line_color => '#00FF00'}
-      :qmax => { :line_color => '#00FF00'},
+      :qcur => { :line_color => '#00FF00'},
+      :qmax => { :line_color => '#FFaa33'},
       :scur => { :line_color => '#00FF00'},
-      :slim => { :line_color => '#00FF00'},
+      :slim => { :line_color => '#0F0F0F'},
       :stot => { :line_color => '#00FF00'},
-      :bin => { :line_color => '#00FF00'},
-      :bout => { :line_color => '#00FF00'},
-      :hrsp_1xx => { :line_color => '#00FF00'},
-      :hrsp_2xx => { :line_color => '#00FF00'},
-      :hrsp_3xx => { :line_color => '#00FF00'},
-      :hrsp_4xx => { :line_color => '#00FF00'},
-      :hrsp_5xx => { :line_color => '#00FF00'},
-      :hrsp_other => { :line_color => '#00FF00'},
-      :req_tot => { :line_color => '#00FF00'},
       :conn_tot => { :line_color => '#00FF00'}
     }
-    []
   end
 end
-
