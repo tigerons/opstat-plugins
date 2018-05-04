b815a5e9536e1b455981750c97f18ec5cc53880d
tigerons <37334768+tigerons@users.noreply.github.com>
2018-05-04 12:17:29 +0200

    Update haproxy.rb

--- lib/app/models/haproxy.rb
+++ lib/app/models/haproxy.rb
@@ -21,35 +21,18 @@ class Haproxy
             where(:timestamp.lt => options[:end]).
             where(:host_id => options[:host_id]).
             where(:plugin_id => options[:plugin_id]).order_by(timestamp: :asc)
-   guides = []
-   unless  host_facts.nil?
-   guide = {}
-   require 'ruby-units'
-      guide[:value] = 'forntend-http'
-      guide[:value_axis] = 'valueAxis1'
-      guide[:line_color] = "#00FFaa33"
-      guide[:line_thickness] = 1
-      guide[:dash_length] = 5
-      guide[:label] = "Total physical"
-      guide[:inside] = true
-      guide[:line_alpha] = 1
-      guide[:position] = 'bottom'
-      guides << guide
 
-  end
-   chart[:guides] = guides
    chart
 
  end
 
   def self.graphs
-    {
-      :qcur => { :line_color => '#00FF00'},
-      :qmax => { :line_color => '#FFaa33'},
-      :scur => { :line_color => '#00FF00'},
-      :slim => { :line_color => '#0F0F0F'},
-      :stot => { :line_color => '#00FF00'},
-      :conn_tot => { :line_color => '#00FF00'}
+    { 
+      :stot => { :line_color => '#00FFAC'},
+      :conn_tot => { :line_color => '#C0FF00'},
+      :bin => { :line_color => '#FCA0FF'}
     }
   end
 end
+~                                                                                                                                                                                                                                             
+~          
