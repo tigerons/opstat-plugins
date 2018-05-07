dd8443290444b59861c321b42c694429ede78676
tigerons <37334768+tigerons@users.noreply.github.com>
2018-05-07 14:55:53 +0200

    Update haproxy.rb

--- lib/app/models/haproxy.rb
+++ lib/app/models/haproxy.rb
@@ -2,37 +2,41 @@ class Haproxy
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
   include Mongoid::Timestamps
-  include Graphs::AreaNotStackedChart
+  include Graphs::AreaStackedChart
   store_in collection: "opstat.reports"
   field :timestamp, type: DateTime
-  index({timestamp: 1, host_id: 1, plugin_id: 1}, {background: true})
+  index({timestamp: 1, host_id: 1, plugin_id: 1})
 
   def self.chart_data(options = {})
     charts = []
-    charts << self.haproxy_chart(options)
+    charts = self.haproxy_charts(options)
     return charts
   end
 
+  def self.haproxy_charts(options)
+    charts = []
+    Disk.where(:timestamp.gte => options[:start]).
+         where(:timestamp.lt => options[:end]).
+         where(:host_id => options[:host_id]).
+         where(:plugin_id => options[:plugin_id]).
+         order_by(timestamp: :asc).group_by{|u| u.svname}.each_pair do |mount, values|
+      charts << self.haproxy_chart(mount, values)
+    end
+    return charts
+  end
 
-  def self.haproxy_chart(options)
-   chart = self.chart_structure({:title => 'Haproxy test', :value_axis => { :title => "Session per second"}})
-
-   chart[:graph_data] = Haproxy.where(:timestamp.gte => options[:start]).
-            where(:timestamp.lt => options[:end]).
-            where(:host_id => options[:host_id]).
-            where(:plugin_id => options[:plugin_id]).order_by(timestamp: :asc)
-
-   chart
-
- end
+  def self.haproxy_chart(mount, values)
+    chart = self.chart_structure({:title => "Haproxy #{mount}", :value_axis => { :title => "Haproxy"}})
+    #TODO - get fields from above DRY
+    chart[:graph_data] = values
+    chart
+  end
 
   def self.graphs
-    { 
-      :stot => { :line_color => '#00FFAC'},
+    {
+        :stot => { :line_color => '#00FFAC'},
       :conn_tot => { :line_color => '#C0FF00'},
       :bin => { :line_color => '#FCA0FF'}
-    }
+}
   end
 end
-~                                                                                                                                                                                                                                             
-~          
