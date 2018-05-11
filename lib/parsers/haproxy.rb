e34fafa0b6236dd887207564bbfd90e0913ecd1c
tigerons <37334768+tigerons@users.noreply.github.com>
2018-05-11 23:57:44 +0200

    Update haproxy.rb

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -8,12 +8,14 @@ module Parsers
     def parse_data(data_parse)
       p data_parse
       white_headers = [:_pxname, :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout, :hrsp_1xx, :hrsp_2xx, :hrsp_3xx, :hrsp_4xx, :hrsp_5xx, :hrsp_other, :req_tot, :conn_tot]
+      frontend_values = ["frontend", "frontends"]
       hashed_data = {}
       reports = []
       data_parse.split.each do |word|
       CSV.parse(word.join, { headers: true, header_converters: :symbol, converters: :all}) do |row|
       hashed_data = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
-      reports <<  hashed_data.select {|key, value| white_headers.include?(key) }
+        hashed_data.select {|key, value| frontend_values.include?(value)} #:svname=>frontend :svname=>frontends
+        reports <<  hashed_data.select {|key, value| white_headers.include?(key) }
       end
       end
       return reports
@@ -21,4 +23,3 @@ module Parsers
   end
 end
 end
-
