1ea250feeface59add3d18e09f93fe54705218d8
tigerons <37334768+tigerons@users.noreply.github.com>
2018-05-18 11:48:56 +0200

    Update haproxy.rb

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -11,30 +11,25 @@ module Parsers
         :frontends =>  [],
         :backends =>   []
       }
-
-require 'pp'
-  data = CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
-  data.each { | key, value|
-    case
-      when value.first[:svname] == "FRONTEND"
-        reports[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
-      when value.first[:_pxname].to_s.start_with?("backend")
-        backends = { :name => value.first[:_pxname] }
-        rep = {}
-        value.each do |backend|
-          if backend[:svname].to_s.start_with?("BACKEND")
-            backends[:summary] =  backend.select { |key, value| white_headers.include?(key)}
-          else
-            backend.map do |key, value|
-              rep[key] = value
-              backends[:details] =  rep.select { |key, value| white_headers.include?(key)}
+      data = CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
+      data.each { | key, value|
+      case
+        when value.first[:svname] == "FRONTEND"
+          reports[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
+        when value.first[:_pxname].to_s.start_with?("backend")
+          backends = { :name => value.first[:_pxname] }
+          array_details= []
+          value.each do |backend|
+            if backend[:svname].to_s.start_with?("BACKEND")
+              backends[:summary] =  backend.select { |key, value| white_headers.include?(key) }
+            else
+              backends[:details] = array_details.push ( backend.select { |key, value| white_headers.include?(key) } )
             end
           end
-            end
-       end
-         reports[:backends] << backends
-     }
-  return reports
+      end
+    reports[:backends] << backends
+      }
+    return reports
     end
 end
 end
