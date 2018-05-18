1cd631e297870e0ceec76a202af45557fbb211e4
tigerons <37334768+tigerons@users.noreply.github.com>
2018-05-18 08:26:03 +0200

    create hash  two keys :backends :frontends

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -6,30 +6,34 @@
   6     include Opstat::Logging
   7 
   8     def parse_data(data_parse)
-  9       white_headers = [ :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout, :hrsp_1xx, :hrsp_2xx, :hrsp_3xx, :hrsp_4xx, :hrsp_5xx, :hrsp_other, :req_tot    , :conn_tot]
- 10       hashed_data = {}
- 11       reports = []
- 12       frontends = {}
- 13       reports_backends = []
- 14       reports_frontends = []
- 15       reports_summary_backends = []
- 16       backends = {}
- 17 
- 18       CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}) do |row|
- 19         hashed_data = [Hash[row.headers[0..-1].zip(row.fields[0..-1])]]
- 20 
- 21         hashed_data.select { |hash| hash[:svname] == "FRONTEND" }.each do |frontend_instances|
- 22           frontends[:frontends] =  { :name => frontend_instances[:_pxname], :summary => frontend_instances.select { |key, value| white_headers.include?(key) }}
- 23         end
- 24 
- 25           hashed_data.select { |hash| hash[:svname] == "BACKEND" }.each do |backend_summary|
- 26             backends[:backends] = { :name => backend_summary[:_pxname], :summary => backend_summary.select { |key, value| white_headers.include?(key) }}
- 27 end
-
- 29 end
- 30 return frontends.merge(backends)
- 31 end
- 32 end
- 33 end
- 34 end
+  9       white_headers = [ :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout, :hrsp_1xx, :hrsp_2xx, :hrsp_3xx, :hrsp_4xx, :hrsp_5xx, :hrsp_other, :req_tot, :conn_tot]
+ 10       reports = {
+ 11         :frontends =>  [],
+ 12         :backends =>   []
+ 13       }
+ 16     data = CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
+ 17     data.each { | key, value|
+ 18       case
+ 19         when value.first[:svname] == "FRONTEND"
+ 20           reports[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
+ 21         when value.first[:_pxname].to_s.start_with?("backend")
+ 22           backends = { :name => value.first[:_pxname] }
+ 23           rep = []
+ 24           value.each do |backend|
+ 25             if backend[:svname].to_s.start_with?("BACKEND")
+ 26               backends[:summary] =  backend.select { |key, value| white_headers.include?(key)}
+ 27             else
+ 28               backends[:details] = value.first.select { |key, value| white_headers.include?(key)}
+ 29             end
+ 30     reports[:backends] << backends
+ 32             end
+ 33           end
+ 34          } 
+ 35     return reports
+ 36     end
+ 37 end 
+ 38 end 
+ 39 end 
+ 40  
+ 41  
 
