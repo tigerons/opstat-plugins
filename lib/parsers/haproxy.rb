d5631f57f12b5710034ec38e0bef95eb0d8ec0a0
tigeron <msawczuk@ifirma.pl>
2018-05-18 14:56:04 +0200

    Update haproxy.rb create array with two hashes backends and frontends

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -7,15 +7,18 @@ module Parsers
 
     def parse_data(data_parse)
       white_headers = [ :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout, :hrsp_1xx, :hrsp_2xx, :hrsp_3xx, :hrsp_4xx, :hrsp_5xx, :hrsp_other, :req_tot, :conn_tot]
-      reports = {
+      report = {
         :frontends =>  [],
         :backends =>   []
       }
-      data = CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
+      require 'pp'
+      pp data_parse
+      p data_parse.class
+      data = CSV.parse(data_parse.join, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
       data.each { | key, value|
       case
         when value.first[:svname] == "FRONTEND"
-          reports[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
+          report[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
         when value.first[:_pxname].to_s.start_with?("backend")
           backends = { :name => value.first[:_pxname] }
           instances = []
@@ -26,12 +29,14 @@ module Parsers
               backends[:details] = instances.push ( backend.select { |key, value| white_headers.include?(key) } )
             end
       end
-    reports[:backends] << backends
+    report[:backends] << backends
     end
       }
-    return reports
+    return [report]
     end
   end
 end
 end
 
+
+~ 
