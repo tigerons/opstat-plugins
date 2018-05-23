50249e72e86b9edec0fdfcb5428724e5afa0dee5
Matthew <msawczuk@power.com.pl>
2018-05-23 07:54:44 +0200

    Haproxy parser

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -7,17 +7,17 @@ module Parsers
 
     def parse_data(data_parse)
       white_headers = [ :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout, :hrsp_1xx, :hrsp_2xx, :hrsp_3xx, :hrsp_4xx, :hrsp_5xx, :hrsp_other, :req_tot, :conn_tot]
-      report = {
-        :frontends =>  [],
-        :backends =>   []
-      }
-      data = CSV.parse(data_parse.join, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
-      data.each do | key, value|
+      report = {}, {}
+     
+      
+
+      data = CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
+      data.each do | key, value |
         case
           when value.first[:svname] == "FRONTEND"
-            report[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
+            report[0] =  {:stats_type => :frontend, :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
           when value.first[:_pxname].to_s.start_with?("backend")
-            backends = { :name => value.first[:_pxname] }
+            backends = { :stats_type => :backend, :name => value.first[:_pxname] }
             instances = []
             value.each do |backend|
               if backend[:svname].to_s.start_with?("BACKEND")
@@ -26,12 +26,13 @@ module Parsers
                 backends[:details] = instances.push ( backend.select { |key, value| white_headers.include?(key) } )
               end
             end
-          report[:backends] << backends
+          report[1] =  backends
         end
       end
-    return [report]
-    end
+    return report
+   end
   end
 end
 end
+
 
