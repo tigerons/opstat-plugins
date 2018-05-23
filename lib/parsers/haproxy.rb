cb474e144e7a5a93419cf7565d1b10c792e93ba8
tigerons <37334768+tigerons@users.noreply.github.com>
2018-05-23 07:20:38 +0200

    Update haproxy.rb

--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -13,24 +13,25 @@ module Parsers
       }
       data = CSV.parse(data_parse.join, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
       data.each do | key, value|
-	case
-	  when value.first[:svname] == "FRONTEND"
-	    report[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
-	  when value.first[:_pxname].to_s.start_with?("backend")
-	    backends = { :name => value.first[:_pxname] }
-	    instances = []
-	    value.each do |backend|
-	      if backend[:svname].to_s.start_with?("BACKEND")
-		backends[:summary] =  backend.select { |key, value| white_headers.include?(key) }
-	      else
-		backends[:details] = instances.push ( backend.select { |key, value| white_headers.include?(key) } )
-	      end
-	    end
-	  report[:backends] << backends
-	end
+        case
+          when value.first[:svname] == "FRONTEND"
+            report[:frontends] << { :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
+          when value.first[:_pxname].to_s.start_with?("backend")
+            backends = { :name => value.first[:_pxname] }
+            instances = []
+            value.each do |backend|
+              if backend[:svname].to_s.start_with?("BACKEND")
+                backends[:summary] =  backend.select { |key, value| white_headers.include?(key) }
+              else
+                backends[:details] = instances.push ( backend.select { |key, value| white_headers.include?(key) } )
+              end
+            end
+          report[:backends] << backends
+        end
       end
-      return [report]
+    return [report]
     end
   end
 end
-end 
+end
+
