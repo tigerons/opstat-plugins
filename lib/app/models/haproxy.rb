f305b6a3dd4af6dfbd9c9d91fc2adfa54f68813a
Matthew <msawczuk@power.com.pl>
2018-06-26 14:14:22 +0200

    Haproxy view

--- lib/app/models/haproxy.rb
+++ lib/app/models/haproxy.rb
@@ -14,73 +14,45 @@ class Haproxy
     return charts
   end
 
-  def self.frontends_haproxy_charts(options)
-    charts = []
-    Haproxy.where(:timestamp.gte => options[:start]).
-            where(:timestamp.lt => options[:end]).
-            where(:host_id => options[:host_id]).
-            where(:plugin_id => options[:plugin_id]).
-              order_by(timestamp: :asc).group_by{ |haproxy| haproxy.frontends }.each_pair do |haproxy_name, values|
-                charts << self.haproxy_chart(haproxy_name, values)
-              end
-    return charts
-  end
-
-  def self.backends_haproxy_charts(options)
-    charts = []
-    Haproxy.where(:timestamp.gte => options[:start]).
-            where(:timestamp.lt => options[:end]).
-            where(:host_id => options[:host_id]).
-            where(:plugin_id => options[:plugin_id]).
-              order_by(timestamp: :asc).group_by{ |haproxy| haproxy.backends }.each_pair do |haproxy_name, values|
-                haproxy_name.each do |backend_instance|
-                charts << self.haproxy_chart(haproxy_name, values)
-              end
-    return charts
-  end
-   
   def self.haproxy_charts(options)
+    haproxy_instance =[]
     charts = []
     Haproxy.where(:timestamp.gte => options[:start]).
             where(:timestamp.lt => options[:end]).
             where(:host_id => options[:host_id]).
             where(:plugin_id => options[:plugin_id]).
-              order_by(timestamp: :asc).group_by{ |haproxy| haproxy.frontends }.each_pair do |haproxy_name, values|
-                haproxy_name.each do |frontend_instance|
-                  charts << self.haproxy_chart(frontend_instance, values)
-                end
+              order_by(timestamp: :asc).group_by{|u| u[:name]}.each_pair do |instance, values|
+                charts << self.haproxy_chart(instance, values)
               end
-    return charts
+            return charts
   end
 
-
-  def self.haproxy_chart(name, values)
-    chart = self.chart_structure({:title => "Haproxy #{frontend_instance[:name]}", :value_axis => { :title => "Session per second"}})
+  def self.haproxy_chart(instance, values)
+    chart = self.chart_structure({:title => "Haproxy #{instance}", :value_axis => { :title => "Session per second"} })
     prev = {}
     chart_data = {}
     instances = Hash.new
     values.each do |value|
-      instances[value[:svname]] ||= true
-      unless prev.has_key?(value[:svname]) then
-        prev[value[:svname]] = value
+      instances[value[:stot]] ||= true
+      unless prev.has_key?(value[:stot]) then
+        prev[value[:stot]] = value
         next
       end
-      time_diff = value[:timestamp].to_i - prev[value[:svname]][:timestamp].to_i
+      time_diff = value[:timestamp].to_i - prev[value[:stot]][:timestamp].to_i
       chart[:graph_data] << {
-      "timestamp" => value[:timestamp].to_i,
-      "session_per_sec" => ((value[:stot].to_i - prev[value[:svname]][:stot].to_i)/time_diff).to_i,
+        "timestamp" => value[:timestamp],
+        "session_per_sec" => ((value[:summary][:stot].to_i - prev[value[:stot]][:summary][:stot].to_i)/time_diff).to_i
       }
-      prev[value[:svname]] = value
+      prev[value[:stot]] = value
     end
     chart
   end
-  def self.instances_graph(instances)
-    return self.chart_structure({ :title => "Transactions per second #{instances} instances", :value_axis => { :title => "Number of sessions"}})
-  end
 
   def self.graphs
-    {
+    { 
       :session_per_sec => { :line_color => '#00FFAC', :line_thickness => 4, :title => "session_per_second", :hidden => false},
     }
   end
+end 
+
 
--- lib/parsers/haproxy.rb
+++ lib/parsers/haproxy.rb
@@ -1,7 +1,7 @@
 require 'csv'
+require 'yaml'
 module Opstat
 module Parsers
-
   class Haproxy
     include Opstat::Logging
 
@@ -9,7 +9,8 @@ module Parsers
       white_headers = [ :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout, :hrsp_1xx, :hrsp_2xx, :hrsp_3xx, :hrsp_4xx, :hrsp_5xx, :hrsp_other, :req_tot, :conn_tot]
       data = CSV.parse(data_parse, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
       report = []  
-      data.each do |key, value|
+      yaml = []
+      data.each do |key,value|
         case
           when value.first[:svname] == "FRONTEND"
             report <<  {:stats_type => :frontend, :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
@@ -26,10 +27,14 @@ module Parsers
 	  report <<  backends
         end
       end
-      return report 
+      puts report 
+      out_file = File.open("haproxy.yml", "w")
+      report.each do |record| 
+          out_file.puts(record.to_yaml)
+      end
+      return report
     end
   end
 end
-end
-
+end 
 
